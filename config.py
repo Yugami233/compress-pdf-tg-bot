17dd09c0b8ac37686901f129191a4514e6758384
Yugami233 <120727508+Yugami233@users.noreply.github.com>
2023-01-01 14:36:03 +0600

    Update and rename sample_config.py to config.py

--- config.py
+++ config.py
@@ -11,13 +11,13 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 class Config(object):
     # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5231238723:AAFppyPBD3VKzj_-kHMXquwEVv1fAcOJCl4")
 
     # Get from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", ""))
+    APP_ID = int(os.environ.get("APP_ID", "5948230"))
 
     # Get from my.telegram.org
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "dd19a00b085a219421a3717d0ae9c0d0")
 
 
 def LOGGER(name: str) -> logging.Logger:
